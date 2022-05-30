6a3a178d4aa5dcf509812d9822c2420358108b32
KikiLoLxD <84983409+KikiLoLxD@users.noreply.github.com>
2022-05-30 21:33:20 +0200

    Add files via upload

---
+++ maturski/skripte/meni.js
@@ -0,0 +1,26 @@
+function panel1() {
+    var x = document.getElementById("panel1");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function panel2() {
+    var x = document.getElementById("panel2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function panel3() {
+    var x = document.getElementById("panel3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
